1c1abae23bc995b6d08bfe5eee1eceb0192bcd6c
maben01 <www.maben@foxmail.com>
2019-11-24 19:22:31 +0800

    optimize bitmap

--- bitmap.h
+++ bitmap.h
@@ -7,7 +7,7 @@
 #include "memory.h"
 
 #ifndef TS_BITMAP_PREALLOC
-#define TS_BITMAP_PREALLOC 128
+#define TS_BITMAP_PREALLOC 128LL
 #endif
 
 typedef struct {
@@ -18,6 +18,7 @@ typedef struct {
 #define ts_bitmap_p(bm) ((ts_bitmap_t *)(bm))
 
 #define ts_bitmap_init(bm) (bzero((void *)(bm), sizeof(ts_bitmap_t)))
+
 #define ts_bitmap_destroy(bm) { \
     if (ts_bitmap_p(bm)->l > 0) ts_free(ts_bitmap_p(bm)->c); \
 }
@@ -29,32 +30,29 @@ typedef struct {
     memcpy(ts_bitmap_p(dst)->c, ts_bitmap_p(src)->c, ts_bitmap_p(src)->l); \
 }
 
-#define ts_bmp_align(s) ((s + 127) & ~0x7f)
+#define ts_bmp_align(s) ((s + TS_BITMAP_PREALLOC - 1) & ~(TS_BITMAP_PREALLOC - 1))
 #define ts_bmp_dig(p) ((p) >> 0x3)
 #define ts_bmp_mod(p) ((p) & 0x7)
 
 #define ts_bitmap_reserve(bm, n) { \
-    if ((n) >= (bm)->l) { \
-        uint64_t __old = (bm)->l; \
-        uint64_t __len = (bm)->l + (((n) - (bm)->l) < TS_BITMAP_PREALLOC ? \
-                TS_BITMAP_PREALLOC : ts_bmp_align((n) - (bm)->l)); \
+    if ((n) > ((bm)->l)) {  \
+        uint64_t __len = ts_bmp_align(n); \
         (bm)->c = ts_realloc((bm)->c, __len); \
         assert((bm)->c != NULL); \
-        memset((void *)((bm)->c + __old), 0x0, __len - __old); \
+        bzero((void *)((bm)->c + (bm)->l), __len - (bm)->l); \
         (bm)->l = __len; \
     } \
 }
 
 #define ts_bitmap_set(bm, p) { \
-    ts_bitmap_reserve(ts_bitmap_p(bm), ts_bmp_dig(p)); \
+    ts_bitmap_reserve(ts_bitmap_p(bm), ts_bmp_dig(p) + 1); \
     ts_bitmap_p(bm)->c[ts_bmp_dig(p)] |= (1 << ts_bmp_mod(p)); \
 }
 
 #define ts_bitmap_unset(bm, p) { \
-    ts_bitmap_reserve(ts_bitmap_p(bm), ts_bmp_dig(p)); \
-    ts_bitmap_p(bm)->c[ts_bmp_dig(p)] &=  ~(1 << (ts_bmp_mod(p))); \
+    if (ts_bmp_dig(p) < (bm)->l) ts_bitmap_p(bm)->c[ts_bmp_dig(p)] &=  ~(1 << (ts_bmp_mod(p))); \
 }
 
-#define ts_bitmap_get(bm, p)  ((ts_bmp_dig(p) >= ((ts_bitmap_p(bm))->l) ? 0 : \
+#define ts_bitmap_get(bm, p) ((ts_bmp_dig(p) >= ((ts_bitmap_p(bm))->l) ? 0 : \
             ((ts_bitmap_p(bm)->c[ts_bmp_dig(p)]) >> ts_bmp_mod(p))) & 1)
 
--- tests/bitmap_test.c
+++ tests/bitmap_test.c
@@ -2,28 +2,52 @@
 #include <strings.h>
 #include "bitmap.h"
 
+#define TEST_COUNT 10240
 int main() {
-    ts_bitmap_t bm;
+    ts_bitmap_t bm, clone;
     ts_bitmap_init(&bm);
-    int i;
-    for (i = 0; i < 5120; i++) {
-        ts_bitmap_set(&bm, i);
-    }
-    for (i = 0; i < 512; i++) {
-        printf("bit:%d = %d\n", i, ts_bitmap_get(&bm, i));
-    }
-    for (i = 0; i < 512; i++) {
-        if (i % 2 == 0) {
-            ts_bitmap_unset(&bm, i); 
+    char *testdata = malloc(TEST_COUNT);
+    bzero(testdata, TEST_COUNT);
+    int i, r;
+
+    for (i = 0; i < TEST_COUNT; i++) {
+        if (i & 1) {
+            ts_bitmap_set(&bm, i);
+            testdata[i] = 1;
         }
     }
-    for (i = 0; i < 513; i++) {
-        printf("bit:%d = %d\n", i, ts_bitmap_get(&bm, i));
+
+    // unset
+    for (i = 0; i < TEST_COUNT; i++) {
+        if ((i & 3) == 3) {
+            ts_bitmap_unset(&bm, i);
+            testdata[i] = 0;
+        }
     }
-    for (i = 0; i < 1024; i++) {
-        ts_bitmap_unset(&bm, i); 
+
+    // check
+    for (i = 0; i < TEST_COUNT; i++) {
+        if (ts_bitmap_get(&bm, i) == testdata[i]) {
+            printf("i:%d pass, value:%d\n", i, testdata[i]);
+        } else {
+            printf("check fail, i:%d\n", i);
+            exit(1);
+        }
     }
-    ts_bitmap_get(&bm, 2048); 
+
+    ts_bitmap_clone(&bm, &clone);
+    // check clone
+    for (i = 0; i < TEST_COUNT; i++) {
+        if (ts_bitmap_get(&clone, i) == testdata[i]) {
+            printf("clone i:%d pass, value:%d\n", i, testdata[i]);
+        } else {
+            printf("clone check fail, i:%d\n", i);
+            exit(1);
+        }
+    }
+
     ts_bitmap_destroy(&bm);
+    ts_bitmap_destroy(&clone);
+    free(testdata);
     return 0;
 }
